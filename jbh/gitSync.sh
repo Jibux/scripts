34afa770ee547a6e954d3ab027187ee4a21b2486
Jibux <jibux@jibux.info>
2012-07-04 14:55:08 +0200

    update scripts

--- jbh/gitSync.sh
+++ jbh/gitSync.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-path=/home/jbh/Documents/gitSave
-
-cd $path/config
-git commit -m "update conf" ./
-gitSync
-
-cd $path/scripts
-git commit -m "update scripts" ./
-gitSync
-
-cd $path/Development
-git commit -m "update code" ./
-gitSync
+keychain ~/.ssh/id_rsa.github
+git push origin master
 
