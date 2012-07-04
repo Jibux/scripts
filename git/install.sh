4c414152ff78b2cae1afa4fb6acab443d1b841a3
Jibux <jibux@jibux.info>
2012-07-04 16:08:18 +0200

    update scripts

--- git/install.sh
+++ git/install.sh
@@ -3,8 +3,12 @@
 # First, install sudo
 /bin/su - -c 'aptitude install sudo;visudo'
 
+sudo perl -pi -e 's/main/main contrib non-free/g' /etc/apt/sources.list
+sudo echo -e "\ndeb http://backports.debian.org/debian-backports squeeze-backports main contrib non-free" >> /etc/apt/sources.list
+sudo aptitude update
+
 # INSTALL ALL REQUIRED PACKAGES
-sudo aptitude install git xorg fluxbox slim rxvt-unicode xdg-utils vim-gtk screen alsa-utils moc moc-ffmpeg-plugin network-manager libgconf2-4 libxss1 libcurl3 ssh-askpass keychain
+sudo aptitude install git xorg fluxbox slim rxvt-unicode xdg-utils vim-gtk screen alsa-utils moc moc-ffmpeg-plugin network-manager libgconf2-4 libxss1 libcurl3 ssh-askpass keychain flashplugin-nonfree
 
 # PREPARE DIRECTORIES
 mkdir -p ~/Documents/gitSave
