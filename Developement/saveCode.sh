4121f124beeb68d8e3f1e3d26d76a2edfc20652a
Jibux <jibux@jibux.info>
2012-07-28 13:27:09 +0200

    1st commit

---
+++ Developement/saveCode.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+devDirectory="/home/jbh/Documents/Development"
+saveDirectory="/mnt/data/JBH/Informatique/Unix"
+
+rsync -auv $devDirectory/TOSAVE $saveDirectory/
+cd $devDirectory
+for folder in `cat TOSAVE`
+do
+	tar -cvjf $saveDirectory/$folder.tar.bz2 $folder
+done
+
