cb09a860dc0da3ef7c23c7bebcf86f843b0819d4
Jibux <jibux@jibux.info>
2012-07-28 13:41:14 +0200

    add Dev Script comment fiew lines

--- git/update.sh
+++ git/update.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 0 if $1 is not defined
-updateDev=${1:-0}
-
 ###########
 # CONFIGS #
 ###########
@@ -16,11 +13,12 @@ rsync -auv /home/jbh/.bash_profile config/
 rsync -auv /home/jbh/.local/share/applications/defaults.list config/.local/share/applications/
 rsync -auv /home/jbh/.local/share/applications/mimeapps.list config/.local/share/applications/
 rsync -auv /home/jbh/UTILES config/
+rsync -auv /home/jbh/TODO config/
 
-rsync -auv /etc/bash.bashrc config/etc/
+#rsync -auv /etc/bash.bashrc config/etc/
 rsync -auv /etc/DIR_COLORS config/etc/
-rsync -auv /etc/vimrc config/etc/
-rsync -auv /etc/screenrc config/etc/
+#rsync -auv /etc/vimrc config/etc/
+#rsync -auv /etc/screenrc config/etc/
 rsync -auv /etc/rc.d/*custom config/etc/rc.d/
 rsync -auv /etc/rc.conf config/etc/
 rsync -auv /etc/udev/rules.d/11-media-by-label-auto-mount.rules config/etc/udev/rules.d/
@@ -34,6 +32,8 @@ rsync -auv /usr/share/moc/themes/custom_theme config/usr/share/moc/themes/
 rsync -auv --delete /etc/acpi/ scripts/etc/acpi/
 rsync -auv --delete /scripts/ scripts/root/
 rsync -auv --delete /home/jbh/scripts/ scripts/jbh/
+rsync -auv /home/jbh/Documents/Development/saveCode.sh scripts/Developement/
+rsync -auv /home/jbh/Documents/Development/TOSAVE scripts/Developement/
 rsync -auv /home/jbh/Documents/gitSave/update.sh scripts/git/gitUpdate.sh
 rsync -auv /home/jbh/Documents/gitSave/sync.sh scripts/git/gitSync.sh
 rsync -auv /home/jbh/Documents/gitSave/load.sh scripts/git/gitLoad.sh
