5253675727f17880a717a35094c27fa2df142789
Jibux <jibux@jibux.info>
2015-11-05 22:45:40 +0100

    Add new script

---
+++ jbh/clean_disk_remove_duplicates.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#######################################################################
+# Remove duplicate files from a path.                                 #
+# Analyse file after file and compare md5 of a file to the next file. #
+# Then move duplicate files into another directory.                   #
+#######################################################################
+
+FILE_PATH=/media/jbh/WD-JBH/RECUP/
+DOUBLES_PATH=/media/jbh/WD-JBH/RECUP_DOUBLES/
+
+cd ${FILE_PATH}
+
+old_md5=""
+
+ls -r | while read file; do
+	md5=$(md5sum "$file" | awk '{print $1}')
+	if [ "${md5}" == "${old_md5}" ]; then
+		echo "mv ${file}"
+		mv "${file}" ${DOUBLES_PATH}
+	fi
+	old_md5="${md5}"
+done
+
