6f270e7be3fd95cfdcd79562a8242e51895509ee
Jibux <jibux@jibux.info>
2020-07-17 15:02:53 +0200

    Separate change author name

--- global/git/git_change_author.sh
+++ global/git/git_change_author.sh
@@ -21,14 +21,20 @@ git filter-branch --env-filter '
 
 if [ "$GIT_COMMITTER_EMAIL" = "'"$OLD_EMAIL"'" ]
 then
-	export GIT_COMMITTER_NAME="'"$NEW_NAME"'"
 	export GIT_COMMITTER_EMAIL="'"$NEW_EMAIL"'"
 fi
 if [ "$GIT_AUTHOR_EMAIL" = "'"$OLD_EMAIL"'" ]
 then
-	export GIT_AUTHOR_NAME="'"$NEW_NAME"'"
 	export GIT_AUTHOR_EMAIL="'"$NEW_EMAIL"'"
 fi
-' --tag-name-filter cat -- --branches --tags
+if [ "$GIT_COMMITTER_NAME" = "'"$OLD_NAME"'" ]
+then
+	export GIT_COMMITTER_NAME="'"$NEW_NAME"'"
+fi
+if [ "$GIT_AUTHOR_NAME" = "'"$OLD_NAME"'" ]
+then
+	export GIT_AUTHOR_NAME="'"$NEW_NAME"'"
+fi
+' -f --tag-name-filter cat -- --branches --tags
 
 
