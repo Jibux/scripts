80318330d89e9cb37f6deaf5924a91e281902541
Jibux <jibux@jibux.info>
2020-09-18 09:14:11 +0200

    Add all keys to ssh_init

--- global/ssh/ssh_init.sh
+++ global/ssh/ssh_init.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
 
-key="$(ssh-add -l | grep id_rsa)"
+add_key()
+{
+	local key_path=$1
+	local key
+	local key_found
 
-if [ "$key" == "" ]; then
-	ssh-add ~/.ssh/id_rsa
-fi
+	key=$(awk '{print $2}' "$key_path.pub")
+	key_found="$(ssh-add -L | grep "$key")"
+
+	if [ -z "$key_found" ]; then
+		echo "Add key '$key_path'"
+		ssh-add "$key_path"
+	else
+		echo "Key '$key_path' already added"
+	fi
+}
+
+while IFS= read -u 3 -r path; do
+	add_key "$path"
+done 3< <(find "$HOME/.ssh/" -type f -not -name '*.pub' -not -name '*known_hosts*' -not -name '*authorized_keys*' -not -name '*config*')
 
