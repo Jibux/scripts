6891d94c4de0776fd8405e61fed859e1f7cf63ce
Jibux <jibux@jibux.info>
2023-04-10 15:13:30 +0200

    Add fix_rights_for_share.sh

---
+++ global/fix_rights_for_share.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+set -euo errexit
+
+
+exit_fail()
+{
+	echo "${1:-Something wrong happened}" >&2
+	exit 1
+}
+
+usage()
+{
+	echo "Usage: $0 directory [group_mode]"
+	echo "group_mode: ro or rw"
+}
+
+
+[ "$#" -lt "1" ] && usage && exit 1
+
+
+DIR="$1"
+GROUP_MODE="${2:-ro}"
+GROUP_WRITE_MODE=""
+
+[ -d "$DIR" ] || exit_fail "'$DIR' is not a directory"
+
+[ "$GROUP_MODE" == "rw" ] && GROUP_WRITE_MODE="w"
+
+
+chgrp -R parents "$DIR"
+chmod -R o=,u=rwX,"g=rX$GROUP_WRITE_MODE" "$DIR"
+find "$DIR" -type d -exec chmod g+s {} \; -exec setfacl -m d:g::rx$GROUP_WRITE_MODE {} \;
+
